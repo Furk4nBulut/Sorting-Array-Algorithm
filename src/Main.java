befe707f819b0a98eeb5680041e814354da004c5
Furkan Bulut <93736726+Furk4nBulut@users.noreply.github.com>
2022-12-29 02:06:18 +0300

    A simple algorithm for sorting arrays

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] idArray = {100315086, 100315002, 100315079};
+        double [] idArrayDouble = {100315086.444, 100315002.4, 10031.5079};
+        String [] nameArray = {"Ali" , "Zeki" , "Ayşe"};
+        Sort.sortingArrays(idArray,nameArray);
+        Sort.sortingArrays(idArrayDouble,nameArray);
+    }
+}
---
+++ src/Sort.java
@@ -0,0 +1,46 @@
+public class Sort {
+    public static void sortingArrays(int[] idArray, String[] nameArray) {
+        for (int i = 0; idArray.length > i; i++) {
+            int currentMin = idArray[i];
+            String currentString = nameArray[i];
+            int currentIndex = i;
+            for (int j = i + 1; j < idArray.length; j++) {
+                if (currentMin > idArray[j]) {
+                    currentMin = idArray[j];
+                    currentString = nameArray[j];
+                    currentIndex = j;
+                }
+            }
+            if (currentIndex != i) {
+                idArray[currentIndex] = idArray[i];
+                idArray[i] = currentMin;
+                nameArray[currentIndex] = nameArray[i];
+                nameArray[i] = currentString;
+            }
+            System.out.println(i + 1 + "." + idArray[i]);
+            System.out.println(i + 1 + "." + nameArray[i]);
+        }
+    }
+    public static void sortingArrays(double[] idArray, String[] nameArray) {
+        for (int i = 0; idArray.length > i; i++) {
+            double currentMin = idArray[i];
+            String currentString = nameArray[i];
+            int currentIndex = i;
+            for (int j = i + 1; j < idArray.length; j++) {
+                if (currentMin > idArray[j]) {
+                    currentMin = idArray[j];
+                    currentString = nameArray[j];
+                    currentIndex = j;
+                }
+            }
+            if (currentIndex != i) {
+                idArray[currentIndex] = idArray[i];
+                idArray[i] = currentMin;
+                nameArray[currentIndex] = nameArray[i];
+                nameArray[i] = currentString;
+            }
+            System.out.println(i + 1 + "." + idArray[i]);
+            System.out.println(i + 1 + "." + nameArray[i]);
+        }
+    }
+}
